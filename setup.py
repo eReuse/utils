9f01b5f7d525be4dc2633cdced867778234f379a
Xavier Bustamante Talavera <xavier@bustawin.com>
2018-04-17 09:49:59 +0200

    Revert "Add run wrapper"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ereuse-utils",
-    version='0.1.2',
+    version='0.3.0',
     packages=find_packages(),
     url='https://github.com/eReuse/utils',
     license='AGPLv3 License',
