87fd5d9aa2b390bad7fc30cb7fb5f7b6bb5be04b
Xavier Bustamante Talavera <xavier@bustawin.com>
2018-07-07 11:40:24 +0200

    Bump to 0.4.0b2; add cli.Path

--- ereuse_utils/cli.py
+++ ereuse_utils/cli.py
@@ -1,4 +1,5 @@
 import enum as _enum
+import pathlib
 from typing import Type
 
 import click
@@ -14,9 +15,17 @@ class Enum(click.Choice):
     From `this github issue <https://github.com/pallets/click/issues/
     605#issuecomment-277539425>`_.
     """
+
     def __init__(self, enum: Type[_enum.Enum]):
         self.__enum = enum
         super().__init__(enum.__members__)
 
     def convert(self, value, param, ctx):
         return self.__enum[super().convert(value, param, ctx)]
+
+
+class Path(click.Path):
+    """Like click.Path but returning ``pathlib.Path`` objects."""
+
+    def convert(self, value, param, ctx):
+        return pathlib.Path(super().convert(value, param, ctx))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,8 @@
+import pathlib
 from enum import Enum
 
 import click
 from click.testing import CliRunner
-
 from ereuse_utils import cli
 
 
@@ -14,9 +14,22 @@ def test_cli_enum():
     @click.command()
     @click.option('--foo', type=cli.Enum(FooEnum))
     def foo(foo: FooEnum):
-        return print(foo.value)
+        print(foo.value)
 
     runner = CliRunner()
     result = runner.invoke(foo, ('--foo', 'Bar'))
     assert result.exit_code == 0
     assert result.output.strip() == '2'
+
+
+def test_cli_path():
+    @click.command()
+    @click.option('--foo', type=cli.Path())
+    def foo(foo: pathlib.Path):
+        assert isinstance(foo, pathlib.Path)
+        print(foo)
+
+    runner = CliRunner()
+    result = runner.invoke(foo, ('--foo', '/foo/bar'))
+    assert result.exit_code == 0
+    assert result.output.strip() == '/foo/bar'
